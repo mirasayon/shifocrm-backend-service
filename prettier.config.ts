ba34ab213b54143e988e325b4024ea52b8add55e
Miralisher <mirasayon@ya.ru>
2026-03-14 07:26:51 +0500

    configs and needed deps

---
+++ prettier.config.ts
@@ -0,0 +1,12 @@
+import type { Config } from "prettier";
+export default {
+    semi: true,
+    singleQuote: false,
+    trailingComma: "all",
+    printWidth: 150,
+    tabWidth: 4,
+    useTabs: false,
+    bracketSpacing: true,
+    arrowParens: "always",
+    endOfLine: "lf",
+} satisfies Config;
