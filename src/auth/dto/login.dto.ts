0db1b5fc09c1546faea2b77ae37ce9d50f2a77b1
Mira <mirasayon@ya.ru>
2026-03-14 17:39:50 +0500

    format

--- src/auth/dto/login.dto.ts
+++ src/auth/dto/login.dto.ts
@@ -1,12 +1,12 @@
-import { IsString, IsNotEmpty, MinLength } from 'class-validator';
+import { IsString, IsNotEmpty, MinLength } from "class-validator";
 
 export class LoginDto {
-  @IsString()
-  @IsNotEmpty({ message: 'Email yoki telefon raqami kiritilishi shart' })
-  identifier: string;
+    @IsString()
+    @IsNotEmpty({ message: "Email yoki telefon raqami kiritilishi shart" })
+    identifier: string;
 
-  @IsString()
-  @IsNotEmpty({ message: 'Parol kiritilishi shart' })
-  @MinLength(6, { message: 'Parol kamida 6 ta belgidan iborat bo\'lishi kerak' })
-  password: string;
+    @IsString()
+    @IsNotEmpty({ message: "Parol kiritilishi shart" })
+    @MinLength(6, { message: "Parol kamida 6 ta belgidan iborat bo'lishi kerak" })
+    password: string;
 }
